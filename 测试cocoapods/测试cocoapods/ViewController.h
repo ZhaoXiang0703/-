4712929d504e61e1c77e908e1045936ba6383266
zhaoxiang <1171458891@qq.com>
2016-08-29 18:06:22 +0800

    测试的呢

---
+++ 测试cocoapods/测试cocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试cocoapods
+//
+//  Created by 赵响 on 16/8/29.
+//  Copyright © 2016年 赵响. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 测试cocoapods/测试cocoapods/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  测试cocoapods
+//
+//  Created by 赵响 on 16/8/29.
+//  Copyright © 2016年 赵响. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
