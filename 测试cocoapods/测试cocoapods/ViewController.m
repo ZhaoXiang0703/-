725a9ce777783b1a96286ff971584aff2e3146e9
zhaoxiang <1171458891@qq.com>
2016-08-30 09:20:52 +0800

    修改了VC.m

--- 测试cocoapods/测试cocoapods/ViewController.m
+++ 测试cocoapods/测试cocoapods/ViewController.m
@@ -10,18 +10,30 @@
 
 @interface ViewController ()
 
+@property(nonatomic,strong)UIButton *button;
+
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.button=[[UIButton alloc]init];
+    self.button.frame=CGRectMake(100, 100, self.view.frame.size.width-200, 40);
+    [self.button setTitle:@"戳我" forState:UIControlStateNormal];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
